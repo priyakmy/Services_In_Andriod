92efd63ba8c72a62a7eb197c9d5bd7e78aab3f32
priya Kumari <priyakum1071@gmail.com>
2024-04-12 00:00:59 +0530

    commit

---
+++ app/src/main/java/MusicService.kt
@@ -0,0 +1,60 @@
+// MusicService.kt
+import android.app.*
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.servicesinandroidstudio.MainActivity
+
+class MusicService : Service() {
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer.create(this, R.raw.your_music_file)
+        mediaPlayer.isLooping = true
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+        }
+        startForeground(1, createNotification())
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
+    }
+
+    private fun createNotification(): Notification {
+        val channelId = "music_channel"
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                    channelId,
+                    "Music Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+                .setContentTitle("Background Music Player")
+                .setContentText("Playing Music")
+                .setSmallIcon(R.drawable.arrow)
+                .setContentIntent(pendingIntent)
+                .build()
+
+        return notification
+    }
+}
